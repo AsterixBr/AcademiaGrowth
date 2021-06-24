9cefab4622da58e4a22e71881c3b2707897af58e
AsterixBr <xandsbarbosa@gmail.com>
2021-06-24 11:50:56 -0300

    update com php

--- conecta.php
+++ conecta.php
@@ -9,15 +9,24 @@ $link = mysqli_connect($endereco,$usuario,$senha,$banco)
 or die('Erro:'.mysqli_error($link));
 
 $sql = "INSERT INTO registro VALUES";
-$sql .= "'".$_POST['email']."',";
+$sql .= "('".$_POST['email']."',";
 $sql .= "'".$_POST['senha']."',";
 $sql .= "'".$_POST['nome']."',";
-$sql .= "'".$_POST['endereco']."',";
-$sql .= "'".$_POST['endereco']."',";
+$sql .= "'".$_POST['endereço']."',";
+$sql .= "'".$_POST['endereço2']."',";
 $sql .= "'".$_POST['cidade']."',";
 $sql .= "'".$_POST['Estado']."',";
-$sql .= "'".$_POST['CEP']."',";
+$sql .= "'".$_POST['CEP']."')";
 
 $consulta = mysqli_query($link,$sql);
+
+if(!$consulta)
+{
+    die('Erro:' .mysqli_error($link));
+}
+else
+{
+    echo 'Matricula efetuada com sucesso';
+}
 
 ?>
